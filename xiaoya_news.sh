954698bfa5761d5dede90308f11c31524a1a8cdb
iwuyuxue <iwuyuxue@gmail.com>
2023-12-13 01:43:41 +0800

    Update xiaoya_news.sh

--- xiaoya_news.sh
+++ xiaoya_news.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "你好啊"
-ugreenpath=$(docker inspect -f {{.HostConfig.Binds}} xiaoya | tr -d "[" | tr -d "]")
-if[ugreenpath]；then echo “你的文件路径映射为$ugreenpath” 
-if
+ugreenpath=$(docker inspect -f {{.HostConfig.Binds}} xiaoya |tr -d "[" |tr -d "]")
+if[ugreenpath];then echo “你的文件路径映射为$ugreenpath” 
+fi
